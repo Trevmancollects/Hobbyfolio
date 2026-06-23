eb0525c3e2426c440bbe2efdc7e6569958efbb30
Trevor O'Loughlin <trevoroloughlin@Trevors-Mac-mini.local>
2026-06-22 21:17:09 -0400

    PWA install

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,16 @@
+import withPWA from 'next-pwa'
+
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: '*.supabase.co' },
-    ],
+    remotePatterns: [{ protocol: 'https', hostname: '*.supabase.co' }],
   },
 }
 
-export default nextConfig
+export default withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})(nextConfig)
